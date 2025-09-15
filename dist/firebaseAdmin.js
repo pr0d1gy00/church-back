dda62a2173e066550cbe473630ad8c5db5ed294d
prodigy00 <calitoalejandro184@gmail.com>
2025-09-15 00:19:41 -0400

    new credentials notifications work

--- dist/firebaseAdmin.js
+++ dist/firebaseAdmin.js
@@ -4,10 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const firebase_admin_1 = __importDefault(require("firebase-admin"));
-const church_6a68a_firebase_adminsdk_fbsvc_0d9a7f8633_json_1 = __importDefault(require("./church-6a68a-firebase-adminsdk-fbsvc-0d9a7f8633.json"));
-if (!firebase_admin_1.default.apps.length) {
-    firebase_admin_1.default.initializeApp({
-        credential: firebase_admin_1.default.credential.cert(church_6a68a_firebase_adminsdk_fbsvc_0d9a7f8633_json_1.default),
-    });
-}
+var serviceAccount = require("../church-6a68a-firebase-adminsdk-fbsvc-ced6ea89e8.json");
+firebase_admin_1.default.initializeApp({
+    credential: firebase_admin_1.default.credential.cert(serviceAccount),
+});
 exports.default = firebase_admin_1.default;
--- src/firebaseAdmin.ts
+++ src/firebaseAdmin.ts
@@ -1,13 +1,29 @@
 import admin from "firebase-admin";
 import { ServiceAccount } from "firebase-admin";
-
-var serviceAccount = require("../church-6a68a-firebase-adminsdk-fbsvc-ced6ea89e8.json");
-
+import serviceAccount from "../church-6a68a-firebase-adminsdk-fbsvc-804724d52f.json";
+if (!admin.apps.length) {
 admin.initializeApp({
 		credential: admin.credential.cert(
 			serviceAccount as ServiceAccount
 		),
 });
+}
+const registrationToken = 'fqo_mwqLRXWPcjNdB0owMC:APA91bGkorfbqt5vo6GBWLTgT1CYjD3VmQ4jvrR4VQ1_x-v4I3wFK9nfruNRUgjvpbzxqe7J5H9MyGywQQDeUQ58v5C_ju8XaSaCWE3q1b1xJzrRS9xde2M';
 
+const message = {
+  notification: {
+    title: 'Hola',
+    body: 'Notificación de prueba',
+  },
+  token: registrationToken,
+};
+
+admin.messaging().send(message)
+  .then((response) => {
+    console.log('Notificación enviada:', response);
+  })
+  .catch((error) => {
+    console.log('Error enviando notificación:', error);
+  });
 
 export default admin;
