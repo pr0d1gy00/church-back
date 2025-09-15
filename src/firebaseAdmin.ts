d9c49f37c74ec9477195773d765fb251fa8557e9
prodigy00 <calitoalejandro184@gmail.com>
2025-09-15 18:28:50 -0400

    removed notification of testing

--- src/firebaseAdmin.ts
+++ src/firebaseAdmin.ts
@@ -8,22 +8,6 @@ admin.initializeApp({
 		),
 });
 }
-const registrationToken = 'fqo_mwqLRXWPcjNdB0owMC:APA91bGkorfbqt5vo6GBWLTgT1CYjD3VmQ4jvrR4VQ1_x-v4I3wFK9nfruNRUgjvpbzxqe7J5H9MyGywQQDeUQ58v5C_ju8XaSaCWE3q1b1xJzrRS9xde2M';
 
-const message = {
-  notification: {
-    title: 'Hola',
-    body: 'Notificación de prueba',
-  },
-  token: registrationToken,
-};
-
-admin.messaging().send(message)
-  .then((response) => {
-    console.log('Notificación enviada:', response);
-  })
-  .catch((error) => {
-    console.log('Error enviando notificación:', error);
-  });
 
 export default admin;
